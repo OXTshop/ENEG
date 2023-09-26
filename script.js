07fb646eedd4c786162ad70647123a8cb25a788c
OXT <76634863+OXTshop@users.noreply.github.com>
2023-09-26 08:02:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,13 +18,13 @@ function mostrarGrafico(id) {
         // Se for o chart1, use os dados acumulados dos outros gráficos
         data = chart1Data;
         suggestedMax = 600;
-    } else if (id === 'chart2' || id === 'chart3' || id === 'chart4') {
+    } if (id === 'chart2' || id === 'chart3' || id === 'chart4') {
         data = [12, 14, 8, 10, 9, 13, 14, 12, 8];
         suggestedMax = 50;
-    }else if (id == 'chart3'){
+    }if (id == 'chart3'){
         data = [ 6,  9, 4,  8, 4,  6,  9,  5, 4];
         suggestedMax = 50;
-    }else if (id = 'chart4'){
+    }if (id = 'chart4'){
         data = [ 8,  9, 3,  7, 4,  5,  5,  8, 2];
         suggestedMax= 50;
     }
